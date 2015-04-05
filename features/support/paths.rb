b1b6ad62e898588dd1232e9826f1cfa55a775c85
Dinesh Billa <dineshbilla@gmail.com>
2015-04-05 11:10:35 -0500

    Changes for AddSurvey.feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,8 +13,11 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
+    when /^the surveys home\s?page$/
       '/'
+    when /^the Create New Survey page/
+      '/surveys/new'
+
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:

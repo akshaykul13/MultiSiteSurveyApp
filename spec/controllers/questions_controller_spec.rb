078ea4bd12b503a85a7cf126ef8e38296d7367d2
lekk32 <spandanalekkala@gmail.com>
2015-05-11 22:06:14 -0500

    Corrected failing rspec tests for questions controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 
 describe QuestionsController, type: :controller do 
+  include Devise::TestHelpers 
+  include FactoryGirl::Syntax::Methods
   before :each do
+    #DatabaseCleaner.clean
+     @request.env["devise.mapping"] = Devise.mappings[:user]
+     @user = FactoryGirl.create(:user)
+     sign_in @user  
     get :new
   end
   it 'should display new template for rendering' do 
@@ -12,7 +18,12 @@ end
 
 describe QuestionsController, type: :controller do 
  #fixtures :questions 
- before :each do
+  include Devise::TestHelpers 
+  include FactoryGirl::Syntax::Methods
+  before :each do
+     @request.env["devise.mapping"] = Devise.mappings[:user]
+     @user = FactoryGirl.create(:user)
+     sign_in @user 
     #@question1 = FactoryGirl.build(:survey_id => 1, :question => "First question")
     #@question2 = FactoryGirl.build(:survey_id => 2, :question => "Second Question")
     #@fake_questions = [@question1, @question2]
@@ -23,8 +34,13 @@ describe QuestionsController, type: :controller do
   end
 end
 
-describe QuestionsController, type: :controller do 
+describe QuestionsController, type: :controller do
+  include Devise::TestHelpers 
+  include FactoryGirl::Syntax::Methods 
   before :each do
+     @request.env["devise.mapping"] = Devise.mappings[:user]
+     @user = FactoryGirl.create(:user)
+     sign_in @user 
     post :create, {:question => {:survey_id => '1'}}
   end 
   it 'should redirect to show template for rendering' do 
